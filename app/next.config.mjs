79fd8b1820435787c5bc58426f8ab72571ff3faa
Huseyin Taze <huseyintaze105@gmail.com>
2024-03-02 15:37:31 +0300

    Add Product Page

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        images:{
+          remotePatterns:[{hostname: "images.unsplash.com"}]
+        },
+        serverActions: true
+    }
+};
 
 export default nextConfig;
