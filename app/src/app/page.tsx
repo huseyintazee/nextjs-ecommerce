615fb3c51705d89d5c35d9a48a6c454b9f444112
Huseyin Taze <huseyintaze105@gmail.com>
2024-03-14 06:41:09 +0300

    Navbar created

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
                         <h1 className={'text-5xl font-bold'}>{products[0].name}</h1>
                         <p className={'py-6'}>{products[0].description}</p>
                         <Link
-                            href={'/products/'+products[0].id}
+                            href={'/products/' + products[0].id}
                             className={'btn-primary btn uppercase'}
                         >
                             Check it out
