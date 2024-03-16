50a72179f961b57c99907aa21e477ace8a75763a
Huseyin Taze <huseyintaze105@gmail.com>
2024-03-16 23:46:56 +0300

    Update ProductCard.tsx

--- app/src/components/ProductCard.tsx
+++ app/src/components/ProductCard.tsx
@@ -7,6 +7,10 @@ interface ProductCardProps {
     product: Product
 }
 
+function truncate(source: string, size: number) {
+    return source.length > size ? source.slice(0, size - 1) + "…" : source;
+}
+
 export default function ProductCard({product}: ProductCardProps) {
     const isNew = Date.now() - new Date(product.createdAt).getTime() < 1000 * 60 * 60 * 24 * 7;
 
@@ -29,11 +33,15 @@ export default function ProductCard({product}: ProductCardProps) {
                     {product.name}
 
                 </h2>
-                {isNew && <div className={'badge badge-secondary'}>
-                    NEW
-                </div>}
+                {isNew &&
+                    <span className={'badge badge-error text-base-100'}>
+                        NEW
+                    </span>
+                }
                 <p>
-                    {product.description}
+                    {
+                        truncate(product.description, 149)
+                    }
                 </p>
                 <PriceTag price={product.price}/>
             </div>
